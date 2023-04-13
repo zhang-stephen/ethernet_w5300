f561fa7d7c99505c51f995ae8e5a3531fda36262
Stephen Zhang <yicheng123456@outlook.com>
2023-04-14 03:37:27 +0800

    reformat all verilog source files

--- src/top.v
+++ src/top.v
@@ -3,43 +3,43 @@
 // 2023-03-28
 
 module top(
-	// system ports
-	input clk0,   // 50MHz XTAL
-	input rst_n,  // Reset
+        // system ports
+        input clk0,   // 50MHz XTAL
+        input rst_n,  // Reset
 
-	// w5300 ports， without BRDY pins
-	inout tri [15:0] data,
-	input int_n,
-	output [9:0] addr,
-	output cs_n,
-	output rd_n,
-	output we_n,
-	output wrst_n,
-	output rw_n,   // for flip-flop controls
+        // w5300 ports， without BRDY pins
+        inout tri [15:0] data,
+        input int_n,
+        output [9:0] addr,
+        output cs_n,
+        output rd_n,
+        output we_n,
+        output wrst_n,
+        output rw_n,   // for flip-flop controls
 
-	// UART for debugging
-	input uart_rxd,
-	output reg uart_txd,
+        // UART for debugging
+        input uart_rxd,
+        output reg uart_txd,
 
-	// LEDs for status
-	output [3:0] leds
-);
+        // LEDs for status
+        output [3:0] leds
+    );
 
-wire wclk0;
+    wire wclk0;
 
-pll wpll(
-	.inclk0(clk0),
-	.c0(wclk0)
-);
+    pll wpll(
+            .inclk0(clk0),
+            .c0(wclk0)
+        );
 
-led_status led_status_0(
-	.rst_n(rst_n),
-	.clk(clk0),
-	.err_n(),
-	.leds(leds)
-);
+    led_status led_status_0(
+                   .rst_n(rst_n),
+                   .clk(clk0),
+                   .err_n(),
+                   .leds(leds)
+               );
 
-_w5300_parallel_if_rw rw();
+    _w5300_parallel_if_rw rw();
 
 endmodule
 
--- src/w5300/_rw.v
+++ src/w5300/_rw.v
@@ -3,94 +3,109 @@
 // 2023-03-28
 
 module _w5300_parallel_if_rw(
-	// physical ports to w5300
-	input rst_n,
-	input clk,
-	inout tri [15:0] data,
-	output reg [9:0] addr,
-	output reg cs_n,
-	output reg rd_n,
-	output reg we_n,
-	output reg rw_n,
+        // internal signals
+        input rst_n,
+        input clk,
 
-	// logic control ports
-	input [10:0] c_addr,
-	input [15:0] c_idata,
-	output reg [15:0] c_odata,
-	output reg rw_ready		// pull down for r/w operation is ongoing
-);
+        // control ports
+        input [10:0] c_addr,
+        input [15:0] c_idata,
+        output reg [15:0] c_odata,
+        output reg rw_ready,		// pull down for r/w operation is ongoing
 
-localparam S_RW_IDLE = 3'd0, S_RW_ADDR_SETUP = 3'd1, S_RW_CTRL_SETUP = 3'd2, S_RW_DATA_SETUP = 3'd3, S_RW_CPLT = 3'd4;
-localparam ADDR_OP_RD = 1'b1, ADDR_OP_WR = 1'b0;
-localparam KEEP_TICKS = 4'd4; // all signal keeps for 4 ticks(40ns for 100MHz)
+        // physical ports
+        inout tri [15:0] data,
+        output reg [9:0] addr,
+        output reg cs_n,
+        output reg rd_n,
+        output reg we_n,
+        output reg rw_n
+    );
 
-// Internal Module Registers(all starts with a leading underline)
-reg [11:0] _addr;
-reg [15:0] _data;
-reg [7 :0] _state;
-reg [3 :0] _cnt;
+    localparam S_RW_IDLE = 3'd0, S_RW_ADDR_SETUP = 3'd1, S_RW_CTRL_SETUP = 3'd2, S_RW_DATA_SETUP = 3'd3, S_RW_CPLT = 3'd4;
+    localparam ADDR_OP_RD = 1'b1, ADDR_OP_WR = 1'b0;
+    localparam KEEP_TICKS = 4'd2; // all signal keeps for 2 ticks(20ns for 100MHz)
 
-always @* begin
-	_addr = c_addr;
-end
+    // Internal Module Registers(all starts with a leading underline)
+    reg [11:0] _addr;
+    reg [15:0] _data;
+    reg [7 :0] _state;
+    reg [3 :0] _cnt;
 
-assign data = _data;
+    always @*
+        begin
+            _addr = c_addr;
+        end
 
-always @(posedge clk or negedge rst_n)
-	if (!rst_n) begin
-		cs_n <= 1'b1;
-		rd_n <= 1'b1;
-		we_n <= 1'b1;
-		rw_n <= 1'b1;
-		rw_ready <= 1'b1;
-		addr <= 10'bzz_zzzz_zzzz;
-		_data <= 16'bzzzz_zzzz_zzzz;
-		_state <= S_RW_IDLE;
-		_cnt <= 4'd0;
-	end
-	else case (_state)
-		S_RW_IDLE: begin
-			_state <= S_RW_ADDR_SETUP;
-			_data <= 16'bzzzz_zzzz_zzzz;
-			_cnt <= 4'd0;
-			cs_n <= 1'b1;
-			rd_n <= 1'b1;
-			we_n <= 1'b1;
-			rw_n <= 1'b1;
-			rw_ready <= 1'b1;
-			addr <= 10'bzz_zzzz_zzzz;
-		end
+    assign data = _data;
 
-		S_RW_ADDR_SETUP: begin
-			addr <= _addr[9:0];
-			rw_ready = 1'b0;
-			_state <= S_RW_CTRL_SETUP;
-		end
+    always @(posedge clk or negedge rst_n)
+        if (!rst_n)
+            begin
+                cs_n <= 1'b1;
+                rd_n <= 1'b1;
+                we_n <= 1'b1;
+                rw_n <= 1'b1;
+                rw_ready <= 1'b1;
+                addr <= 10'bzz_zzzz_zzzz;
+                _data <= 16'bzzzz_zzzz_zzzz_zzzz;
+                _state <= S_RW_IDLE;
+                _cnt <= 4'd0;
+            end
+        else
+            case (_state)
+                S_RW_IDLE:
+                    begin
+                        _state <= S_RW_ADDR_SETUP;
+                        _data <= 16'bzzzz_zzzz_zzzz_zzzz;
+                        _cnt <= 4'd0;
+                        cs_n <= 1'b1;
+                        rd_n <= 1'b1;
+                        we_n <= 1'b1;
+                        rw_n <= 1'b1;
+                        rw_ready <= 1'b1;
+                        addr <= 10'bzz_zzzz_zzzz;
+                    end
 
-		S_RW_CTRL_SETUP: begin
-			cs_n <= 1'b0;
-			rw_n <= _addr[10];
-			rd_n <= _addr[10] == ADDR_OP_RD ? 1'b0 : 1'b1;
-			we_n <= _addr[10] == ADDR_OP_WR ? 1'b0 : 1'b1;
-			_state <= S_RW_DATA_SETUP;
-		end
+                S_RW_ADDR_SETUP:
+                    begin
+                        addr <= _addr[9:0];
+                        rw_ready <= 1'b0;
+                        _state <= S_RW_CTRL_SETUP;
+                    end
 
-		S_RW_DATA_SETUP: begin
-			_state <= S_RW_CPLT;
+                S_RW_CTRL_SETUP:
+                    begin
+                        cs_n <= 1'b0;
+                        rw_n <= _addr[10];
+                        rd_n <= _addr[10] == ADDR_OP_RD ? 1'b0 : 1'b1;
+                        we_n <= _addr[10] == ADDR_OP_WR ? 1'b0 : 1'b1;
+                        _state <= S_RW_DATA_SETUP;
+                    end
 
-			case (_addr[10])
-				ADDR_OP_RD: c_odata <= _data;
-				ADDR_OP_WR: _data <= c_idata;
-			endcase
-		end
+                S_RW_DATA_SETUP:
+                    begin
+                        _state <= S_RW_CPLT;
 
-		S_RW_CPLT: begin
-			if (_cnt < KEEP_TICKS) _cnt = _cnt + 1'b1;
-			else _state <= S_RW_IDLE;
-		end
+                        case (_addr[10])
+                            ADDR_OP_RD:
+                                c_odata <= _data;
+                            ADDR_OP_WR:
+                                _data <= c_idata;
+                        endcase
+                    end
 
-		default: _state <= S_RW_IDLE;
-	endcase
+                S_RW_CPLT:
+                    begin
+                        if (_cnt < KEEP_TICKS)
+                            _cnt <= _cnt + 1'b1;
+                        else
+                            _state <= S_RW_IDLE;
+                    end
+
+                default:
+                    _state <= S_RW_IDLE;
+            endcase
 
 endmodule
 
